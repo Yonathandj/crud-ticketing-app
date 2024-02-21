83ead05046873611f7718ae7185e9ec674553878
Yonathan Dawang Jati <yonathandawang111@gmail.com>
2024-02-21 20:39:26 +0700

    feat: add file and method to connect to db

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import connection.ConnectDatabase;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try(Connection connection = ConnectDatabase.connectDB()) {
+
+            System.out.println("Connected");
+        } catch (SQLException err) {
+            System.out.println(err.getMessage());
+        }
     }
 }
---
+++ src/connection/ConnectDatabase.java
@@ -0,0 +1,16 @@
+package connection;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.DriverManager;
+
+public class ConnectDatabase {
+    public static Connection connectDB() {
+        try {
+            return DriverManager.getConnection(System.getenv("DB_URL"), System.getenv("DB_USERNAME"), System.getenv("DB_PASSWORD"));
+        } catch (SQLException err) {
+            System.out.println(err.getMessage());
+        }
+        return null;
+    }
+}
